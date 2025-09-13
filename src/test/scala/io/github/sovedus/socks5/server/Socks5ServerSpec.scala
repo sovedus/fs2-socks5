3e0540b14a1d3609e4d9509adbbb66d726583b8a
sovedus <sovedus@mail.ru>
2025-09-13 19:19:14 +0500

    fix(server): add license header

--- src/test/scala/io/github/sovedus/socks5/server/Socks5ServerSpec.scala
+++ src/test/scala/io/github/sovedus/socks5/server/Socks5ServerSpec.scala
@@ -1,16 +1,35 @@
+/*
+ * Copyright 2025 Sovedus
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.sovedus.socks5.server
+
+import io.github.sovedus.socks5.server.auth.NoAuthAuthenticator
+import io.github.sovedus.socks5.test.utils.Handshake
 
 import cats.effect.{IO, Resource}
 import cats.effect.testing.scalatest.AsyncIOSpec
-import com.comcast.ip4s.{IpAddress, IpLiteralSyntax, Port, SocketAddress}
-import fs2.{Chunk, Pipe}
-import fs2.io.net.Network
-import io.github.sovedus.socks5.server.auth.NoAuthAuthenticator
-import io.github.sovedus.socks5.test.utils.Handshake
+
 import org.scalatest.flatspec.AsyncFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.typelevel.log4cats.LoggerFactory
 import org.typelevel.log4cats.noop.NoOpFactory
+
+import com.comcast.ip4s.{IpAddress, IpLiteralSyntax, Port, SocketAddress}
+import fs2.{Chunk, Pipe}
+import fs2.io.net.Network
 
 class Socks5ServerSpec extends AsyncFlatSpec with AsyncIOSpec with Matchers {
   private implicit val loggerFactory: LoggerFactory[IO] = NoOpFactory[IO]
